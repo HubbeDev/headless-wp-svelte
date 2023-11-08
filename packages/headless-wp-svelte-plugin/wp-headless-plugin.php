2a2f174f33675b8b75be3ec0eaa7619a8448d306
HubbeDev <privat@responsivhub.com>
2023-11-08 15:46:54 +0100

    Fix wrong package linking

---
+++ packages/headless-wp-svelte-plugin/wp-headless-plugin.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Plugin Name: Wordpress headless svelte plugin
+ * Plugin URI:  https://github.com/HubbeDev/headless-wp-svelte
+ * Description: A plugin to help turn WordPress into a headless CMS.
+ * Version:     0.0.1
+ * Author:      HubbeDev <info@responsivhub.com>
+ * Author URI:  https://github.com/HubbeDev
+ * License:     MIT
+ *
+ * @package WP_SVELTE_PLUGIN
+ */
+
+namespace WP_SVELTE_PLUGIN;
+
+// Exit if accessed directly.
+if (!defined('ABSPATH')) {
+    die;
+}
